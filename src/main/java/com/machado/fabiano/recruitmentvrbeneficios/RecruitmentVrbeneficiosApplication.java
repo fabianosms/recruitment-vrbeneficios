fcd2a610480839d5db42c0f57c207f12dd45c85b
Fabiano Machado <contato@fabianomachado.com>
2022-09-11 15:39:07 -0300

    Initial commit

---
+++ src/main/java/com/machado/fabiano/recruitmentvrbeneficios/RecruitmentVrbeneficiosApplication.java
@@ -0,0 +1,13 @@
+package com.machado.fabiano.recruitmentvrbeneficios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentVrbeneficiosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmentVrbeneficiosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/machado/fabiano/recruitmentvrbeneficios/RecruitmentVrbeneficiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machado.fabiano.recruitmentvrbeneficios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentVrbeneficiosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
